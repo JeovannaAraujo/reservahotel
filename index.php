1535225f976f1126d9be9f6908392c0e798a7253
JeovannaAraujo <137370011+JeovannaAraujo@users.noreply.github.com>
2024-05-13 19:33:02 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        h2 {
+            text-align: center;
+        }
+
+        a {
+            display: block;
+            width: 400px;
+            padding: 10px;
+            margin: 10px auto;
+            text-align: center;
+            text-decoration: none;
+            color: #fff;
+            background-color: #3498db;
+            border-radius: 10px;
+        }
+
+        a:hover {
+            background-color: #2980b9;
+        }
+    </style>
+</head>
+<body>
+    <br>
+    <h1>Bem Vindo!</h1>
+    <h2>Escolha uma das opções:</h2>
+
+    <a href="../reservahotel/efetuaReserva.php">Efetuar Reservas</a>
+    <a href="../reservahotel/cancelaReserva.php">Cancelamento de reservas</a>
+
+</body>
+</html>
